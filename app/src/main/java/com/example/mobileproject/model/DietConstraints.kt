784201a03eee005937163fab1f7fff46ea76571b
Raziqrr <rzqrdzn03@gmail.com>
2025-05-15 11:05:22 +0800

    add diet constraints

---
+++ app/src/main/java/com/example/mobileproject/model/DietConstraints.kt
@@ -0,0 +1,20 @@
+/**
+ * @Author: Raziqrr rzqrdzn03@gmail.com
+ * @Date: 2025-05-15 10:45:25
+ * @LastEditors: Raziqrr rzqrdzn03@gmail.com
+ * @LastEditTime: 2025-05-15 10:51:08
+ * @FilePath: app/src/main/java/com/example/mobileproject/model/DietConstraints.kt
+ * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+ */
+package com.example.mobileproject.model
+
+class DietConstraints {
+    val user:User = User()
+    var goal: String
+    val targetTime: String
+    val physicalIntensity: String
+    val sleepPattern: Int
+    val eatingPattern: Int
+    val cookingAbility: String
+    val budgetConstraints: String
+}
